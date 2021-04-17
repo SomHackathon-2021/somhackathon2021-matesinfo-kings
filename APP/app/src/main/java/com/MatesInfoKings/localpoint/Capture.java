f591b10db5aeaf47386c13484a133ef0bb0584e8
mikisf <mikisalafranci@gmail.com>
2021-04-17 17:58:37 +0200

    Scanner

---
+++ APP/app/src/main/java/com/MatesInfoKings/localpoint/Capture.java
@@ -0,0 +1,7 @@
+package com.MatesInfoKings.localpoint;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+
+}
--- APP/app/src/main/java/com/MatesInfoKings/localpoint/MainActivity.java
+++ APP/app/src/main/java/com/MatesInfoKings/localpoint/MainActivity.java
@@ -2,15 +2,25 @@ package com.MatesInfoKings.localpoint;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,11 +29,33 @@ public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
+    TextView nPoints;
+    Button btScan;
+    private int points = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nPoints = findViewById(R.id.textPoints);
+        String newText = String.valueOf(points) + nPoints.getText();
+        nPoints.setText(newText);
+        btScan = findViewById(R.id.btScan);
+
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(
+                        MainActivity.this
+                );
+                intentIntegrator.setPrompt("For flash use volume up key");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+            }
+        });
 
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
@@ -43,6 +75,33 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(
+                requestCode,resultCode,data
+        );
+        if(intentResult.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    MainActivity.this
+            );
+            builder.setTitle("Resultat Escanejat");
+            String resultat = intentResult.getContents();
+            builder.setMessage(resultat);
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.show();
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "OOPS... No has escanejat res", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
     private void prepareViewPager(ViewPager viewPager, ArrayList<String> arrayList, ArrayList<Fragment> fragmentList) {
         MainAdapter adapter = new MainAdapter(getSupportFragmentManager());
         for (int i=0; i<arrayList.size(); i++){
@@ -55,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<String> arrayList = new ArrayList<>();
         ArrayList<Fragment> fragmentList = new ArrayList<>();
-
 
         public void addFragment(Fragment fragment, String title){
             arrayList.add(title);
