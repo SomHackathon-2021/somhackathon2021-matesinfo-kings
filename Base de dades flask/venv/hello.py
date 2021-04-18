b6f524330f0ce39f317bcf1391ab8eea9e8ea322
Joel <joelhurtadomoreno20@gmail.com>
2021-04-18 11:44:12 +0200

    Implementat generació qr i base de dades

---
+++ Base de dades flask/venv/hello.py
@@ -0,0 +1,64 @@
+from flask import Flask
+
+
+
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+
+def add_data(id,punts):
+    con = get_db()
+    c = con.cursor()
+    c.execute("INSERT INTO qr (qr_id,punts) VALUES (%i,%i)" %(id,punts))
+    con.commit()
+    print("done")
+
+def read_data():
+    con = get_db()
+    c = con.cursor()
+    c.execute("SELECT * FROM qr")
+    rows = c.fetchall()
+
+    for row in rows:
+        print(row)
+    
+
